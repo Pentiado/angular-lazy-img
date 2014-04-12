c60e6a499e3b0df951cc18fefc04fc2254981506
Pawel Wszola <wszola.p@gmail.com>
2014-04-12 18:58:35 +0200

    Extract helpers to separate module

---
+++ angular-lazy-img-helpers.js
@@ -0,0 +1,50 @@
+angular.module('angularLazyImg', []).factory('lazyImgHelpers', [
+  '$window', function($window){
+    'use strict';
+
+    function getWinDimensions(){
+      return {
+        height: $window.innerHeight,
+        width: $window.innerWidth
+      };
+    }
+
+    function isElementInView(elem, offset, win) {
+      var rect = elem.getBoundingClientRect();
+      var bottomline = win.height + offset;
+      return (
+       rect.left >= 0 && rect.right <= win.width + offset && (
+         rect.top >= 0 && rect.top <= bottomline ||
+         rect.bottom <= bottomline && rect.bottom >= 0 - offset
+        )
+      );
+    }
+
+    // http://remysharp.com/2010/07/21/throttling-function-calls/
+    function throttle(fn, threshhold, scope) {
+      var last, deferTimer;
+      return function () {
+        var context = scope || this;
+        var now = +new Date(),
+            args = arguments;
+        if (last && now < last + threshhold) {
+          clearTimeout(deferTimer);
+          deferTimer = setTimeout(function () {
+            last = now;
+            fn.apply(context, args);
+          }, threshhold);
+        } else {
+          last = now;
+          fn.apply(context, args);
+        }
+      };
+    }
+
+    return {
+      isElementInView: isElementInView,
+      getWinDimensions: getWinDimensions,
+      throttle: throttle
+    };
+
+  }
+]);
